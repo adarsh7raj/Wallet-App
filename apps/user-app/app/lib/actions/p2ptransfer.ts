3566f55c42cb1e092e46fa02404b414cdd229f54
unknown <adarshrajyadav68@gmail.com>
2024-12-02 18:47:55 +0530

    Prisma client imported

--- apps/user-app/app/lib/actions/p2ptransfer.ts
+++ apps/user-app/app/lib/actions/p2ptransfer.ts
@@ -2,7 +2,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
-
+import {Prisma} from '@prisma/client'
 export async function p2pTransfer(to: string, amount: number) {
     const session = await getServerSession(authOptions);
     const from = session?.user?.id;
@@ -27,7 +27,7 @@ export async function p2pTransfer(to: string, amount: number) {
     // (e.g., checking a balance before transferring money) or when you need conditional logic between queries.
     // These transactions are sequential transaction they occur one after the other
 
-    await prisma.$transaction(async (tx) => {
+    await prisma.$transaction(async (tx:Prisma.TransactionClient) => {
         await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
         const fromBalance = await tx.balance.findUnique({
             where: { userId: Number(from) }, // 
