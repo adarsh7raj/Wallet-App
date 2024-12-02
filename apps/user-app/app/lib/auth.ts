5d89f9274bc528ed3696a5ab62b208eae9ea4f6b
unknown <adarshrajyadav68@gmail.com>
2024-12-03 00:54:09 +0530

    added suspense element

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -43,7 +43,7 @@ export const authOptions = {
     ],
     secret: process.env.JWT_SECRET || "secret",
     callbacks: {
-        // TODO: can u fix the type here? Using any is bad
+       
         async session({ token, session }: any) {
             session.user.id = token.sub
 console.log(session);
